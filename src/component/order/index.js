f658a16233d4bb916a874b4e1474830221a63455
saqibfaisal <saqibfaisalofficial@gmail.com>
2023-12-01 00:15:35 -0800

    cors remove

--- src/component/order/index.js
+++ src/component/order/index.js
@@ -38,9 +38,10 @@ function Order() {
   const [customerDetailData, setCustomerDetailData] = useState({});
   // const socket = io("ws://localhost:4000", {
   useEffect(() => {
-    const socket = io("https://zameer-ansari-backend.vercel.app", {
-      withCredentials: true,
-    });
+    const socket = io("https://zameer-ansari-backend.vercel.app");
+    // const socket = io("https://zameer-ansari-backend.vercel.app", {
+    //   withCredentials: true,
+    // });
     socket.on("connect", () => {
       console.log("socket connect ", socket.id);
     });
